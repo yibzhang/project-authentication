de3aaa6d149a1020ede24c8a2892039a3d577050
yibzhang <zhangyiboturku@gmail.com>
2019-08-29 16:50:27 +0300

    1 file added, add fake-backend, not finished.

---
+++ src/_backend/fake-backend.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable, throwError, of} from 'rxjs';
+import { delay, mergeMap, materialize, dematerialize } from 'rxjs/operators';
+
+let users = [{id:1 , email: "test1@test.com", password: "12345678"}];
+@Injectable()
+export class FakeBackendInterceptor implements HttpInterceptor{
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
+    const {url, method, headers, body} = request;
+
+
+
+    return of(null).pipe(
+      
+    );
+
+
+
+    function authenticate(){
+      const {email, password} = body;
+      const user = users.find(x => x.email === email && x.password === password);
+      return user? okResponse(200, {id: user.id, email: user.email, token: 'fake-jwt-token'}) : errorResponse;      
+    }
+
+    function okResponse(res_number: number, res_body?){
+      return of(new HttpResponse({status: res_number, body: res_body}));
+    }
+
+    function errorResponse(err_message: string){
+      return throwError(err_message);
+    }
+
+  }
+}
