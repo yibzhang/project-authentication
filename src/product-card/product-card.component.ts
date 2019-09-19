8a38810c3c67ddb78601832e9bf0466eb5c9fe60
yibzhang <zhangyiboturku@gmail.com>
2019-09-19 12:21:27 +0300

    4 files modified, add some icons

--- src/product-card/product-card.component.ts
+++ src/product-card/product-card.component.ts
@@ -1,26 +1,32 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
+import { Product } from './../_model/product';
+
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css']
 })
 export class ProductCardComponent implements OnInit {
-  @Input('imgSrc')
-  imgSrc: string;
+  @Input('product')
+  product: Product;
 
   private productFormGroup = this.formBuilder.group({
-    productNum: new FormControl(''),
+    quantity: new FormControl(''),
   });
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {
+    this.product = new Product();
+  }
 
   ngOnInit() {    
+    this.product.name = "samsung tv";
+    this.product.src = "https://cdn.verk.net/960/images/40/2_491556-2000x1437.jpeg";
   }
 
   addToCart(){
-    console.log(`Add to cart number: ${this.productFormGroup.controls['productNum'].value}`)
+    console.log(`Add to cart number: ${this.productFormGroup.controls['quantity'].value}`)
   }
 
 }
